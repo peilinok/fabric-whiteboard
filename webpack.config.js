0ca5d35d42fd54a737af170d58204874911bb37a
sylar <sylar.huang@all-dream.com>
2020-04-07 16:06:43 +0800

    add support for scss

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
 
