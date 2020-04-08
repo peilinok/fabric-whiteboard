8a36ac256320716aa92477accd2eec2bbdb8f8ff
sylar <sylar.huang@all-dream.com>
2020-04-08 11:09:56 +0800

    add loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+            },
+          },
+        ],
+      },
     ],
   },
 
